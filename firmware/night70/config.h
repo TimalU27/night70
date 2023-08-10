55bf94a1a788072bacfcf946df07370a159b9640
Andrew Lin <landrew051127@gmail.com>
2023-08-10 14:17:13 +0800

    add compiled firmware with VIA compatibility

--- firmware/night70/config.h
+++ firmware/night70/config.h
@@ -17,7 +17,6 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6 }
 #define MATRIX_COL_PINS { F7, C7, C6, D7, D6, D5, D4, D3, D2, D1, D0, B7, B6, B5, B4, B3, B2 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
@@ -29,7 +28,7 @@
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
